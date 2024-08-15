065c5f2ef204ada3837f3b705a7dfb258ebef5de
YmCris <ymcrisystems@gmail.com>
2024-08-14 22:47:18 -0600

    Creación de repositorio

---
+++ Forcade/src/main/java/com/ymcris/ipc1/practica1/forcade/Forcade.java
@@ -0,0 +1,12 @@
+package com.ymcris.ipc1.practica1.forcade;
+
+/**
+ *
+ * @author crist
+ */
+public class Forcade {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
